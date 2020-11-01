af916476715108fada94f2079cd27848e30c5532
justinehell <hell.justine@gmail.com>
2020-11-01 15:17:41 +0100

    gatsby - part 1 - Creating the Order Page with  Cutsom Hooks for form

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
 
 export default function OrderPage() {
+  const { values, updateValue } = useForm({
+    name: '',
+    email: '',
+  });
   return (
     <>
       <SEO title="Order a Pizza !" />
-      <p>Hey! I'm the Order page</p>
+      <form>
+        <fieldset>
+          <legend>Your Info</legend>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={updateValue}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={updateValue}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>Menu</legend>
+        </fieldset>
+        <fieldset>
+          <legend>Order</legend>
+        </fieldset>
+      </form>
     </>
   );
 }
