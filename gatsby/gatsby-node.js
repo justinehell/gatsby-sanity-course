5eddd9b7cc2951978fe18215a8880d7f10559d4e
justinehell <hell.justine@gmail.com>
2020-11-01 13:10:31 +0100

    gatsby - Single Slicemaster Pages

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -105,7 +105,16 @@ async function turnSlicemastersIntoPages({ graphql, actions }) {
     }
   `);
   // 2. turn each slicemaster into their own page
-  // TODO : next video
+  data.slicemasters.nodes.forEach((slicemaster) => {
+    actions.createPage({
+      component: path.resolve('./src/templates/Slicemaster.js'),
+      path: `/slicemaster/${slicemaster.slug.current}`,
+      context: {
+        name: slicemaster.person,
+        slug: slicemaster.slug.current,
+      },
+    });
+  });
   // 3. figure out how many pages there are based on how many slices there are ans how many per page
   const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
   const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
