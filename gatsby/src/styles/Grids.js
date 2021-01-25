4c686513913b88f60ecc62ed0d6d16fb5e5a4cc2
Justine <hell.justine@gmail.com>
2021-01-25 14:22:30 +0100

    making website responsive

--- gatsby/src/styles/Grids.js
+++ gatsby/src/styles/Grids.js
@@ -3,7 +3,11 @@ import styled from 'styled-components';
 export const HomePageGrid = styled.div`
   display: grid;
   gap: 2rem;
-  grid-template-columns: repeat(2, minmax(auto, 1fr));
+  --columns: 2;
+  grid-template-columns: repeat(var(--columns), minmax(auto, 1fr));
+  @media (max-width: 800px) {
+    --columns: 1;
+  }
 `;
 
 export const ItemsGrid = styled.div`
@@ -21,10 +25,14 @@ export const ItemStyles = styled.div`
     font-size: 0;
   }
   p {
-    transform: rotate(-2deg) translateY(-140%);
+    transform: rotate(-2deg) translateY(-10px);
     position: absolute;
     width: 100%;
+    margin: 0;
+    top: 0;
     left: 0;
+    font-size: 2rem; // -> backup if browsers don't recognize clamp
+    font-size: clamp(12px, 5vw, 20px);
   }
   .mark {
     display: inline;
